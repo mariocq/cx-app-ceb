efa1ed649a369637b5d655a305c9ad566e246bbe
MarioMu <mfm@vip.qq.com>
2018-12-24 10:33:16 +0800

    API地址配置化

--- src/services/faceService.js
+++ src/services/faceService.js
@@ -1,11 +1,9 @@
 import request from '../utils/request'
+import IP from "./ip";
 
 export function fetchImage(values) {
-  return request('http://120.79.44.187:5100/api/face-detect', {
+  return request(IP.domain + '/api/face-detect', {
     method: 'POST',
-    headers: {
-      "Content-Type": "application/json"
-    },
     body: JSON.stringify(values),
   });
 }
