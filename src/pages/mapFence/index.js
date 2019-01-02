034f6fcf8b0dd9c396a9176a6c42e09a262d3888
MarioMu <mfm@vip.qq.com>
2019-01-02 14:58:42 +0800

    围栏界面按钮、点和范围示例

--- src/pages/mapFence/index.js
+++ src/pages/mapFence/index.js
@@ -1,9 +1,91 @@
-import React, { Component } from 'react'
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { MapView } from 'react-native-baidumap-sdk'
-
-import { Initializer } from 'react-native-baidumap-sdk'
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Initializer, MapView } from 'react-native-baidumap-sdk';
 Initializer.init().catch(e => console.error(e));
+
+export default class MapStatus extends Component {
+  static navigationOptions = { title: '电子围栏' }
+
+  ZGC = {
+    overlook: 0,
+    rotation: 0,
+    zoomLevel: 16,
+    center: {
+      latitude: 29.6307385629,
+      longitude: 106.6039620327,
+    },
+  }
+
+  TAM = {
+    overlook: 0,
+    rotation: 0,
+    zoomLevel: 14,
+    center: {
+      latitude: 29.6462534314,
+      longitude: 106.5794634119,
+    },
+  }
+
+  toZGC = () => this.setState(this.ZGC)
+  toTAM = () => this.setState(this.TAM)
+
+  render() {
+    return (
+      <View style={styles.full}>
+        <MapView style={styles.full} ref={ref => this.mapView = ref} {...this.state}
+          center={{ latitude: 29.6462534314, longitude: 106.5794634119 }}
+        >
+          {/* 记录点示意 */}
+          <MapView.Marker
+            color="#2ecc71"
+            title="日期：2019-01-02 14:51:38"
+            coordinate={{ latitude: 29.6463534314, longitude: 106.5794634119 }}
+          />
+          <MapView.Marker
+            color="#2ecc71"
+            title="日期：2019-01-02 14:51:38"
+            coordinate={{ latitude: 29.6564534324, longitude: 106.5694634119 }}
+          />
+          <MapView.Marker
+            color="#2ecc71"
+            title="日期：2019-01-02 14:51:38"
+            coordinate={{ latitude: 29.6665534334, longitude: 106.5594634119 }}
+          />
+          <MapView.Marker
+            color="#2ecc71"
+            title="日期：2019-01-02 14:51:38"
+            coordinate={{ latitude: 29.6766534344, longitude: 106.5494634119 }}
+          />
+          <MapView.Marker
+            color="#2ecc71"
+            title="日期：2019-01-02 14:51:38"
+            coordinate={{ latitude: 29.6862534354, longitude: 106.5394634119 }}
+          />
+
+          <MapView.Circle
+            center={{ latitude: 29.6462534314, longitude: 106.5794634119 }}
+            radius={5000}
+            strokeWidth={2}
+            strokeColor="rgba(0, 0, 255, 0.5)"
+            fillColor="rgba(255, 0, 0, 0.5)"
+          />
+        </MapView>
+        <View style={styles.buttons}>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={this.toZGC}>
+              <Text style={styles.text}>一号仓库</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={this.toTAM}>
+              <Text style={styles.text}>二号4S店</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   full: {
@@ -29,50 +111,3 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
 })
-
-export default class MapStatus extends Component {
-  static navigationOptions = { title: 'Map status' }
-
-  ZGC = {
-    overlook: -45,
-    rotation: 90,
-    zoomLevel: 16,
-    center: {
-      latitude: 39.97837,
-      longitude: 116.31363,
-    },
-  }
-
-  TAM = {
-    overlook: 0,
-    rotation: 0,
-    zoomLevel: 14,
-    center: {
-      latitude: 39.90864,
-      longitude: 116.39745,
-    },
-  }
-
-  toZGC = () => this.setState(this.ZGC)
-  toTAM = () => this.setState(this.TAM)
-
-  render() {
-    return (
-      <View style={styles.full}>
-        <MapView style={styles.full} ref={ref => this.mapView = ref} {...this.state} />
-        <View style={styles.buttons}>
-          <View style={styles.button}>
-            <TouchableOpacity onPress={this.toZGC}>
-              <Text style={styles.text}>中关村</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.button}>
-            <TouchableOpacity onPress={this.toTAM}>
-              <Text style={styles.text}>天安门</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    )
-  }
-}
