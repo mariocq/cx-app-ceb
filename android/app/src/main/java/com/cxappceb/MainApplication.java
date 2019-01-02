768e2e2a36633749ea03b6506961f4d11686d5c1
MarioMu <mfm@vip.qq.com>
2019-01-02 11:38:44 +0800

    Baidu地图SDK配置及配套keystore

--- android/app/src/main/java/com/cxappceb/MainApplication.java
+++ android/app/src/main/java/com/cxappceb/MainApplication.java
@@ -3,6 +3,7 @@ package com.cxappceb;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cn.qiuxiang.react.baidumap.BaiduMapPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BaiduMapPackage(),
             new ImagePickerPackage()
       );
     }
